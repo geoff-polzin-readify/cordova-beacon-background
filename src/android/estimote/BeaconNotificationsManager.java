42f5a860f72eb739dc1ae48ca96dd82f86a5746d
Matthew Kindy II <matt.kindy.ii@gmail.com>
2016-09-29 22:17:25 -0500

    Updating plugin structure

--- src/android/estimote/BeaconNotificationsManager.java
+++ src/android/estimote/BeaconNotificationsManager.java
@@ -8,15 +8,27 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.red_folder.phonegap.plugin.backgroundservice.sample.MyService;
+import com.red_folder.phonegap.plugin.backgroundservice.sample.estimote.APIClient;
+
 import com.topl.Topl.MainActivity;
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class BeaconNotificationsManager {
 
@@ -27,14 +39,18 @@ public class BeaconNotificationsManager {
     private List<Region> regionsToMonitor = new ArrayList<>();
     private HashMap<String, String> enterMessages = new HashMap<>();
     private HashMap<String, String> exitMessages = new HashMap<>();
+    private String accessToken = "";
+    private String endpoint = "";
 
     private Context context;
 
     private int notificationID = 0;
 
     public BeaconNotificationsManager(final Context context) {
+
         this.context = context;
         beaconManager = new BeaconManager(context);
+
         beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
             @Override
             public void onEnteredRegion(Region region, List<Beacon> list) {
@@ -51,11 +67,9 @@ public class BeaconNotificationsManager {
                         myservice.setMajorMinor(beacon.getMajor(), beacon.getMinor());
                         Log.d(TAG, "onEnteredRegion: " + beacon.getMajor() + " " + beacon.getMinor());
 
-//                    String message = enterMessages.get(region.getIdentifier());
                         String message = "onEnteredRegion: " + beacon.getMajor()+ " " + beacon.getMinor();
-                        if (message != null) {
-                            showNotification(message);
-                        }
+                        showNotification(message);
+                        updateServer(beacon.getMajor() + "-" + beacon.getMinor());
                     }
                     myservice.setEnabled(false);
                     myservice.stopTimerTask();
@@ -65,7 +79,7 @@ public class BeaconNotificationsManager {
 
             @Override
             public void onExitedRegion(Region region) {
-                // start a 30 sec timer when exit region is triggered
+                // start a 15 sec timer when exit region is triggered
                 MyService myservice = (MyService) context;
                 myservice.onExit();
                 myservice.setDoWorkStatus(false);
@@ -73,17 +87,101 @@ public class BeaconNotificationsManager {
                 myservice.setMilliseconds(15000);
                 myservice.setupTimerTask();
                 Log.d(TAG, "onExitedRegion: " + region.getIdentifier());
-//                String message = exitMessages.get(region.getIdentifier());
-//                if (message != null) {
-//                    showNotification(message);
-//                }
             }
         });
     }
 
+    private void updateServer(String currentRegion) {
+
+        try {
+
+            String[] beacons = {currentRegion};
+
+            RequestParams params = new RequestParams();
+            params.put("beaconIds", beacons);
+
+            APIClient.setBaseUrl(endpoint);
+
+            APIClient.put("/users/me/paymentBeacons", params, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    // If the response is JSONObject instead of expected JSONArray
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                    // Pull out the first event on the public timeline
+                    JSONObject firstEvent = timeline.get(0);
+                    String tweetText = firstEvent.getString("text");
+
+                    // Do something with the response
+                    System.out.println(tweetText);
+                }
+            });
+
+
+
+        } catch(IOException ignored) {}
+
+    }
+
+
+    public static class InputStreamToStringExample {
+
+        public static void main(String[] args) throws IOException {
+
+            // intilize an InputStream
+            InputStream is =
+                    new ByteArrayInputStream("file content..blah blah".getBytes());
+
+            String result = getStringFromInputStream(is);
+
+            System.out.println(result);
+            System.out.println("Done");
+
+        }
+
+        // convert InputStream to String
+        private static String getStringFromInputStream(InputStream is) {
+
+            BufferedReader br = null;
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+
+                br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return sb.toString();
+        }
+
+    }
+
+    public void setToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
     public void addNotification(BeaconID beaconID, String enterMessage, String exitMessage) {
 //        Region region = beaconID.toBeaconRegion();
-        Region region = new Region("My_Beacons", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),null,null);
+        Region region = new Region(beaconID.getProximityUUID().toString(), beaconID.getProximityUUID(), beaconID.getMajor(), beaconID.getMinor());
         enterMessages.put(region.getIdentifier(), enterMessage);
         exitMessages.put(region.getIdentifier(), exitMessage);
         regionsToMonitor.add(region);
